355a2c59dfa667e58c15d3c2d71dc2d71d6cef93
alex fielder <alex.fielder@gmail.com>
2019-03-20 10:31:31 +0000

    getting to grips with looping in Python

--- ModTheMachine/ModTheMachine/spiders/mtmbot.py
+++ ModTheMachine/ModTheMachine/spiders/mtmbot.py
@@ -35,15 +35,15 @@ class MtmbotSpider(scrapy.Spider):
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
-    allowed_domains = ['https://modthemachine.typepad.com/']
-    start_urls = ['https://modthemachine.typepad.com//my_weblog//2019//03//automate-creation-of-named-geometry.html']
+    # allowed_domains = ['https://modthemachine.typepad.com/']
+    # start_urls = ['https://modthemachine.typepad.com//my_weblog//2019//03//automate-creation-of-named-geometry.html']
 
     def parse(self, response):
         #Extracting the content using css selectors
         titles = response.css('.entry-header::text').extract()
         codeSamples = response.css('pre::text').extract()
         times = response.css('.date-header::text').extract()
-        footers = response.css('.post-footers *::text').getall() #response.css('.post-footers::text').extract()
+        # footers = response.css('.post-footers *::text').getall() #response.css('.post-footers::text').extract()
         #this gets the xpath text from the 'post footers' section:
         #response.xpath("//div/div[2]/p[1]/span[1]/a[1]/text()").extract()
         #loop to
@@ -51,7 +51,9 @@ class MtmbotSpider(scrapy.Spider):
         #this gets the text from the footers:
         #response.css('.post-footers *::text').getall()
         #comments = response.css('.comments-content::text').extract()
-       
+		for i = 1 to 20
+			if response.xpath("//div/div[2]/p[1]/span[1]/a[" & i & "]/text()").extract() != "":
+				footers += response.xpath("//div/div[2]/p[1]/span[1]/a[" & i & "]/text()").extract()
         #Give the extracted content row wise
         for item in zip(titles,codeSamples,times,footers):
             #create a dictionary to store the scraped info
